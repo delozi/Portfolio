469c876368cc53ece428a14e7f4e7ec208e010ff
Louis DeLozier <ldeloz1@wgu.edu>
2025-04-01 15:55:45 +0800

    Github Page Support

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,13 @@ const nextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/Portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Portfolio/' : ''
 }
 
 export default withPayload(nextConfig, {
